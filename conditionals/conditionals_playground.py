b154f7a086523051dcdef4bc7a98e3afa446f481
Vanessa <102584523+Nesskim@users.noreply.github.com>
2022-04-30 11:45:46 +1000

    Update conditionals_playground.py

---
+++ conditionals/conditionals_playground.py
@@ -0,0 +1,22 @@
+
+# moths_in_house = True
+moths_in_house = False
+mitch_is_home = True
+# mitch_is_home = False
+
+
+# if
+if moths_in_house and mitch_is_home: 
+    print("Hoooman! Help me get the moths!")
+
+if (not(moths_in_house)) and (not(mitch_is_home)):
+    print("No threats detected.")
+
+if moths_in_house and (not(mitch_is_home)):
+    print("Meooooooooooooow! Hissssss!")
+
+if (not(moths_in_house)) and mitch_is_home:
+    print("Climb on Mitch.")
+
+
+
